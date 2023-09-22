cc1d18e8c8c6ae45cfab286a3a985101e1083d6a
nachocua <nachocuatrin@gmail.com>
2023-09-22 16:44:35 -0300

    Implementación Inicial de la Clase Juego

---
+++ Tp1 - Lab2 - 2023/Tp1 - Lab2 - 2023/Juego.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tp1___Lab2___2023
+{
+    internal class Juego
+    {
+        public int CantidadJugadores { get; private set; }
+        public int CantidadJugadoresActuales { get; private set; }
+        Juego(int cantidadJugadores)
+        {
+            CantidadJugadores = cantidadJugadores;
+        }
+    }
+}
