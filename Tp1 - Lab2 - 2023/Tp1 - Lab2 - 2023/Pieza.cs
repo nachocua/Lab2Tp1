4969a020faca988b057e28ddac1b309b73725924
nachocua <nachocuatrin@gmail.com>
2023-09-22 08:57:17 -0300

    Base Proyect

---
+++ Tp1 - Lab2 - 2023/Tp1 - Lab2 - 2023/Pieza.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1___Lab2___2023
+{
+    internal class Pieza
+    {
+    }
+}
