e6a0af95fb5dddf7b4f988d0c15e30add29cb320
JulianM432 <juliannmisere@gmail.com>
2023-09-23 17:29:42 -0300

    Agregando Juego.cs

--- Tp1 - Lab2 - 2023/Tp1 - Lab2 - 2023/Juego.cs
+++ Tp1 - Lab2 - 2023/Tp1 - Lab2 - 2023/Juego.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,42 @@ namespace Tp1___Lab2___2023
 {
     internal class Juego
     {
+        private bool partidaValida;
+        public string Dificultad { get; private set; }
+        public int NroJuego { get; private set; }
         public int CantidadJugadores { get; private set; }
         public int CantidadJugadoresActuales { get; private set; }
+        ArrayList jugadores;
+        Tablero tablero;
         Juego(int cantidadJugadores)
         {
             CantidadJugadores = cantidadJugadores;
+            tablero = new Tablero();
+        }
+        public void CargarJugador(string nombre, bool humano)
+        {
+            jugadores.Add(new Jugador(nombre));
+        }
+        public void GenerarPiezas()
+        {
+
+        }
+        public void Reset()
+        {
+            tablero.Reset();
+            jugadores.Clear();
+        }
+        public bool IniciarPartida()
+        { 
+            return false; // terminar
+        }
+        public void JugarRonda()
+        {
+
+        }
+        public bool AlguienGano()
+        {
+            return false; // terminar
         }
     }
 }
